95d653cffd3af08fcee7be4185cbd0a104820491
piwo1337 <piwo.owned@gmail.com>
2017-06-12 19:23:37 +0200

    First Commit

---
+++ app/adapters/application.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+import JSONAPIAdapter from 'ember-data/adapters/json-api';
+
+const {String: {pluralize, underscore}} = Ember;
+
+export default JSONAPIAdapter.extend({
+  pathForType(type){
+    return pluralize(underscore(type));
+  },
+  shouldReloadRecord() {
+    return false;
+  },
+
+  shouldBackgroundReloadRecord(store, snapshot) {
+
+    console.log("Calling shouldBackgroundReloadRecord");
+    const loadedAt = snapshot.record.get('loadedAt');
+
+    // if it was loaded more than an hour ago
+    if (Date.now() - loadedAt > 3600000) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+});
